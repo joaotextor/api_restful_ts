c1ac1c7f08029f1da3a92ebca375c7abf7346ee0
Joao Textor <joaotextor@gmail.com>
2023-02-26 09:05:13 -0300

    Remove movie route

--- src/controllers/movieControllers.ts
+++ src/controllers/movieControllers.ts
@@ -28,6 +28,7 @@ export async function getMovieById(req: Request, res: Response) {
         
     } catch (error: any) {
         Logger.error(`Erro no sistema: ${error.message}`)
+        return res.status(500).json('Por favor,tente mais tarde')
     }
 }
 
@@ -44,5 +45,28 @@ export async function getAllMovies(req: Request, res: Response) {
         
     } catch (error: any) {
         Logger.error(`Erro no sistema: ${error.message}`)
+        return res.status(500).json('Por favor,tente mais tarde')
     }
+}
+
+export async function removeMovie(req: Request, res: Response) {
+
+    try {
+
+        const id = req.params.id
+
+        const removedMovie: object | null = await MovieModel.findByIdAndRemove(id)
+
+        if (!removedMovie) {
+            return res.status(404).json({error: "Nenhum filme removido, pois possivelmente não existe!"})
+        }
+
+        return res.status(201).json(`Filme removido com sucesso`)
+        
+    } catch (error: any) {
+        Logger.error(`Erro no sistema: ${error.message}`)
+        return res.status(500).json('Por favor,tente mais tarde')
+    }
+    
+    
 }
--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 import {Router, Request, Response} from "express"
-import { createMovie, getMovieById, getAllMovies } from "./controllers/movieControllers"
+import { createMovie, getMovieById, getAllMovies, removeMovie} from "./controllers/movieControllers"
 
 import {validate} from "./middlewares/handleValidation"
 import { movieCreateValidation } from "./middlewares/movieValidation"
@@ -12,3 +12,4 @@ export default router.get('/test', (req: Request, res: Response) => {
 .post("/movie", movieCreateValidation(), validate, createMovie)
 .get("/movie/:id", getMovieById)
 .get("/movies", getAllMovies)
+.delete("/movie/:id", removeMovie)
